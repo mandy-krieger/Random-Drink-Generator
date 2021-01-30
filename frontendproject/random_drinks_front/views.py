2b2ebc20cc00f009aea91bf1483f07761a713939
mandy-krieger <78228883+mandy-krieger@users.noreply.github.com>
2021-01-30 14:39:20 -0500

    im tryinnnnn

--- frontendproject/random_drinks_front/views.py
+++ frontendproject/random_drinks_front/views.py
@@ -6,3 +6,10 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'random_drinks_front/index.html')
+
+def drink(request):
+
+    user_prompt = str(request.GET["ingred"])
+
+    return render(request, "index.html", {"your drink": name_generator.py})
+
